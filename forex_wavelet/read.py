1a5632ff5f027b394ded14c838e42001307f9d4c
JingjueWei <396600739@qq.com>
2019-03-28 20:07:52 +0800

    Update read.py

--- forex_wavelet/read.py
+++ forex_wavelet/read.py
@@ -5,8 +5,8 @@ import os
 
 
 df = {}
-#name = ['GBP_USD', 'NZD_USD', 'AUD_USD', 'USD_JPY', 'USD_DKK', 'USD_CAD', 'USD_CHF']
-name = ['USD_DKK']
+name = ['GBP_USD', 'NZD_USD', 'AUD_USD', 'USD_JPY', 'USD_DKK', 'USD_CAD', 'USD_CHF']
+
 # read raw data
 for i in range(len(name)):
     df[i] = pd.read_csv(name[i] + ".csv")
@@ -18,11 +18,9 @@ for i in range(len(name)):
     cA_c, cD_c = pywt.dwt(data_c,'db1')
 
     #store data
-    output1=pd.DataFrame([df[i],cA_c],index=['Close_cA'])
-    output2=pd.DataFrame([df[i],cD_c],index=['Close_cD'])
+    output1=pd.DataFrame([df[i],cA_c],index=['Close_cA'],['Close_cD'])
     output1=output1.transpose()
-    output2=output2.transpose()
-    output1.to_csv(name[i]+'_cA.csv',index=False, sep=',')
-    output2.to_csv(name[i]+'_cD.csv',index=False, sep=',')
+    output1.to_csv(name[i]+'.csv',index=False, sep=',')
+   
 
 
